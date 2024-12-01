4398ff8d2cdcecf76eb27af6b158a4e4c44379af
The18Skull <skull18dima@gmail.com>
2024-12-01 22:21:01 +0300

    day 1

---
+++ task1.py
@@ -0,0 +1,17 @@
+# read input
+with open("input1.txt", "r") as f:
+    data = f.read()
+
+left = []; right = []
+for line in data.splitlines():
+    a, b = map(int, line.split(" ", 1))
+    left.append(a); right.append(b)
+
+# part1
+arr1 = sorted(left); arr2 = sorted(right)
+part1 = sum(abs(a - b) for (a, b) in zip(arr1, arr2))
+print(f"part1 = {part1}")
+
+# part2
+part2 = sum([x * right.count(x) for x in left])
+print(f"part2 = {part2}")
